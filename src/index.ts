888012cca05064ce92e31ab903c74c0d108f47c9
Thorsten Lünborg <t.luenborg@googlemail.com>
2023-01-02 22:23:19 +0100

    Components richtig exportieren
    Jede Komponente hat einen default export, der beim re-export mit einem Namen versehen werden muss.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
 import main from './main'
 export default main
-export * from './VTable'
-export * from './VTh'
-export * from './VTr'
-export * from './VTPagination'
-export * from './types'
-
+export { default as VTable } from './VTable'
+export { default as VTh } from './VTh'
+export { default as VTr } from './VTr'
+export { default as VTPagination } from './VTPagination'
